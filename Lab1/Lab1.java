418632ab0fd460742969e8d2ab66c46ab0ac14c5
Alva542 <popovavika175@gmail.com>
2024-11-08 09:42:39 +0100

    MadeChangesJustNow

--- Lab1/Lab1.java
+++ Lab1/Lab1.java
@@ -2,6 +2,7 @@ class Lab1 {
   public static void main(String[] args) {
     System.out.println("Hello pitiful world");
         System.out.println("123456789");
+        System.out.println("MakingChanges");
 
   }
 } 
